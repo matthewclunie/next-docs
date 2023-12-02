4a17154c6483056354246051089b1738650cf5b0
matthewclunie <matthew.clunie@hunterbusinessschool.edu>
2023-12-01 20:02:49 -0500

    form validation, error handling

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -155,7 +155,6 @@ export async function fetchInvoicesPages(query: string) {
 
 export async function fetchInvoiceById(id: string) {
   noStore();
-
   try {
     const data = await sql<InvoiceForm>`
       SELECT
@@ -173,6 +172,7 @@ export async function fetchInvoiceById(id: string) {
       amount: invoice.amount / 100,
     }));
 
+    console.log(invoice);
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
